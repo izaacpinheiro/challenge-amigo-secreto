aa99d06eacb86e9655aad6cef98f6fdb790b1171
izaacpinheiro <izaacpinheiro.123@gmail.com>
2025-08-27 20:42:30 -0300

    implementar resolução do desafio

--- app.js
+++ app.js
@@ -1,1 +1,37 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+// Array de amigos
+let amigos = []
+
+// Função para adicionar amigos no array
+function addAmigo(){
+    let amigo = document.querySelector('input').value;
+    if (amigo == "") {
+        alert("Por favor, insira um nome.");
+    } else {
+        amigos.push(amigo);
+        document.querySelector('input').value = '';
+        atualizarLista();
+    }
+}
+
+// Função para atualizar a lista de amigos
+function atualizarLista() {
+    let lista = document.getElementById('listaAmigos');
+
+    // limpar lista
+    lista.innerHTML = '';
+
+    // adicionar amigos na lista HTML
+    amigos.forEach(nome => {
+        lista.innerHTML += `<li>${nome}</li>`;
+    });
+}
+
+// Função para sortear um amigo aleatorio
+function sortearAmigo() {
+    if (amigos.length > 0) {
+        let indexSorteado = Math.floor(Math.random() * amigos.length);
+        let sorteado = amigos[indexSorteado];
+        let texto = document.getElementById('resultado');
+        texto.innerHTML = sorteado;
+    }
+}
